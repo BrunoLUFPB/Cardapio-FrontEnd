bd1750da2be243c7e9837b98817622a130463480
Bruno Luis <165501730+BrunoLUFPB@users.noreply.github.com>
2025-04-14 02:12:07 -0300

    Função com cadastramento de novos produtos e layout responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import { Card } from './components/card/card';
+import { useState } from 'react'
 import './App.css'
+import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
+
 function App() {
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
+
   return (
-    <div className = "container" >
+    <div className="container">
       <h1>Cardápio</h1>
       <div className="card-grid">
-      {data?.map(foodData => 
-      <Card 
-      price={foodData.price} 
-      title={foodData.title} 
-      image={foodData.image} 
-      description={foodData.description} />)}
+        {data?.map(foodData => 
+          <Card
+            price={foodData.price} 
+            title={foodData.title} 
+            image={foodData.image}
+            description={foodData.description}
+          />
+        )}
+      </div>
+      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+      <button onClick={handleOpenModal}>novo</button>
     </div>
-  </div>
   )
 }
 
